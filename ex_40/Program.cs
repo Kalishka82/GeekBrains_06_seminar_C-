b9654f5fac67de222f47eeccf9be82b19482a767
Tatiana Kalinina <tankakalinka@gmail.com>
2022-09-19 20:51:01 +0300

    add ex 40 Is Triangle Exist

---
+++ ex_40/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может 
+// ли существовать треугольник со сторонами такой длины
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух 
+// других сторон.
+
+Console.Write("Введите длины сторон треугольника через пробел: ");
+string sides = Console.ReadLine()!;
+string[] parts = sides.Split(' ');
+int a = int.Parse(parts[0]);
+int b = int.Parse(parts[1]);
+int c = int.Parse(parts[2]);
+
+if (IsTriangleExist(a, b, c))
+{
+    Console.WriteLine("Треугольник существует");
+}
+else Console.WriteLine("Треугольник НЕ существует");
+
+
+bool IsTriangleExist (int a, int b, int c)
+{
+    return a < b + c && b < c + a && c < a + b;
+}
