dd5f41f38d638854aec1dd1c064d5611fe31d643
Tatiana Kalinina <tankakalinka@gmail.com>
2022-09-19 20:52:05 +0300

    add ex 42 ConvTo2

---
+++ ex_42/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+Console.Write("Input integer number: ");
+int num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(ConvTo2(num));
+
+string ConvTo2(int num)
+{
+    string result = "";
+
+    while (num > 0)
+        {
+            string substring = $"{num % 2}";
+            result = result.Insert(0, substring);
+            num = num / 2;
+        }
+    return result;
+}
